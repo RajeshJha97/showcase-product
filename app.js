1e0a10d9c47571dd69ffa5c07afd55ef0d9ed621
Rajesh <rajesh.jha25@yahoo.com>
2022-08-18 15:48:14 +0530

    initial commit for product

---
+++ app.js
@@ -0,0 +1,41 @@
+const url = 'https://course-api.com/javascript-store-products';
+
+const productsDOM = document.querySelector('.products-center');
+
+const fetchProducts = async () => {
+  productsDOM.innerHTML = `<div class="loading"></div>`;
+  try {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    productsDOM.innerHTML = ` <p class="error">there is an error</p>`;
+  }
+};
+const displayProducts = (list) => {
+  const productList = list
+    .map((product) => {
+      const { id } = product;
+      const { name: title, price } = product.fields;
+      const { url: img } = product.fields.image[0];
+      // console.log(img);
+      //id,name,price,img
+      return ` <a href="product.html?id=${id}" class="single-product">
+            <img src="${img}" class="single-product-img img" alt="${title}" />
+            <footer> 
+              <h5 class="name">${title}</h5>
+              <span class="price">$${price / 100}</span>
+            </footer>
+          </a>`;
+    })
+    .join('');
+  productsDOM.innerHTML = `<div class="products-container">
+          ${productList}
+        </div>`;
+};
+const start = async () => {
+  const data = await fetchProducts();
+  displayProducts(data);
+};
+start();
+//fetchProducts();
---
+++ product.js
@@ -0,0 +1,58 @@
+const productDOM = document.querySelector('.product');
+//console.log(productDOM);
+const url = 'https://course-api.com/javascript-store-single-product';
+
+const fetchProduct = async () => {
+  try {
+    productDOM.innerHTML = `<h4 class="product-loading">Loading...</h4>`;
+    const params = new URLSearchParams(window.location.search);
+    // console.log(params);
+    const id = params.get('id');
+    // console.log(id);
+    const resp = await fetch(`${url}?id=${id}`);
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    productDOM.innerHTML = ` <p class="error">${error}-there is an error in loading the product</p>`;
+  }
+};
+
+const displayProduct = (product) => {
+  //company,colors,description,name:title,price,image,url:image
+  const {
+    company,
+    colors,
+    price,
+    name: title,
+    description,
+    image,
+  } = product.fields;
+  const { url: img } = image[0];
+  // console.log(img);
+  const colorList = colors.map((color) => {
+    return ` <span class="product-color" style="background-color: ${color}"></span>`;
+  });
+  document.title = title.toUpperCase();
+  productDOM.innerHTML = ` <div class="product-wrapper">
+        <img src="${img}" alt="${title}" class="img" />
+        <div class="product-info">
+          <h3>${title}</h3>
+          <h5>${company}</h5>
+          <span>${price / 100}</span>
+          <div class="colors">
+           ${colorList}
+           
+          </div>
+          <p>
+           ${description}
+          </p>
+          <button class="btn">add to cart</button>
+        </div>
+      </div>`;
+};
+
+const start = async () => {
+  const data = await fetchProduct();
+  displayProduct(data);
+};
+start();
